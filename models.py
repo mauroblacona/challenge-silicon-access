92bff71e57a1b36f5ddb09197e5654302d32c945
Mauro Blacona Peralta <>
2024-05-08 16:31:09 -0300

    Models
    Agrego modelo

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, String, Date
+from .db import Base
+
+class Vehiculo(Base):
+    __tablename__ = "vehiculos"
+    id = Column(String, primary_key=True, index=True)
+    tipo = Column(String, index=True)
+    marca = Column(String)
+    modelo = Column(String)
+    color = Column(String)
+    patente = Column(String, unique=True, index=True)
+    aseguradora = Column(String)
+    vencimiento_poliza = Column(Date)
